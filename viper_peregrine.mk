3efb1e80e9b0f482e70182b3308236f00877acfb
ECr34T1v3 <eriikgarciia1999@gmail.com>
2019-08-31 17:39:15 +0000

    peregrine: Its Official ViperOS

--- viper_peregrine.mk
+++ viper_peregrine.mk
@@ -3,6 +3,9 @@ $(call inherit-product, device/motorola/peregrine/full_peregrine.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/viper/config/common_full_phone.mk)
 
+# ViperOS Official flag
+VIPER_BUILD_TYPE := OFFICIAL
+
 PRODUCT_RELEASE_NAME := peregrine
 PRODUCT_NAME := viper_peregrine
 
